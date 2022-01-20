a296fc8d97356a3819ed4cc8d510d0739a5b5d89
ivanmimica <86782522+ivanmimica@users.noreply.github.com>
2022-01-20 15:01:35 +0100

    Added hovered remove task button

--- src/components/todo/Task.tsx
+++ src/components/todo/Task.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { MouseEventHandler } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
@@ -14,34 +15,45 @@ type Props = {
 };
 
 const Task = ({ text, done, toggleTask, deleteTask }: Props & TaskType) => {
+    const [hover, setHover] = useState(false);
+
     const checkedStyle = (done ? '' : 'opacity-50');
     const baseClass = 'flex items-center justify-between p-4 rounded border border-gray-400 bg-stone-200';
     const taskClass = `${baseClass} ${checkedStyle}`;
 
     return (
-        <button
-            onClick={toggleTask}
-        >
-            <div className={taskClass}>
-                <div>
-                    <input
-                        type="checkbox"
-                        checked={done}
-                        readOnly
-                        className="mr-3"
-                    />
-                    <span className="text-gray-600">{text}</span>
+        <div className='relative'>
+            <button
+                onClick={toggleTask}
+                onMouseEnter={() => setHover(true)}
+                onMouseLeave={() => setHover(false)}
+                className="w-full"
+            >
+                <div className={taskClass}>
+                    <div>
+                        <input
+                            type="checkbox"
+                            checked={done}
+                            className="mr-3"
+                        />
+                        <span className="text-gray-600">{text}</span>
+                    </div>
                 </div>
+            </button>
+            {hover &&
                 <button
                     onClick={deleteTask}
+                    onMouseEnter={() => setHover(true)}
+                    onMouseLeave={() => setHover(false)}
+                    className="absolute -right-2 -top-2 opacity-50 hover:opacity-100"
                 >
                     <FontAwesomeIcon
                         icon={faTimesCircle}
                         className="text-orange-500 fa-lg"
                     />
                 </button>
-            </div>
-        </button>
+            }
+        </div>
     );
 };
 
