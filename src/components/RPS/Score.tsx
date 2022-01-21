4cb2f8b854d158de7870b583de99038b0ce5b497
ivanmimica <86782522+ivanmimica@users.noreply.github.com>
2022-01-21 11:02:17 +0100

    Added custom colors to tailwind config

--- src/components/RPS/Score.tsx
+++ src/components/RPS/Score.tsx
@@ -6,14 +6,15 @@ type ScoreType = {
 
 const Score = ({ score }: ScoreType) => {
     return (
-        <div className="flex justify-between p-3 border-4 border-[#606e85] rounded-lg font-sans">
+        <div className="w-full flex justify-between p-3 border-4 border-header-outline rounded-lg font-sans">
             <img
                 src={logo}
+                alt="logo"
                 className="h-14 m-3"
             />
-            <div className='flex flex-col items-center justify-center py-2 px-6 border border-[#11192F] rounded bg-[#FAFAFA]'>
-                <span className='mt-0.5 text-[0.625rem] text-[#2a46c0] font-semibold tracking-widest uppercase leading-none'>score</span>
-                <span className="mt-0.5 mb-1 text-[2.5rem] text-[#3b4363] font-bold leading-none">{score}</span>
+            <div className='flex flex-col items-center justify-center py-2 px-6 border border-[#11192F] rounded bg-white'>
+                <span className='mt-0.5 text-[0.625rem] text-score font-semibold tracking-widest uppercase leading-none'>score</span>
+                <span className="mt-0.5 mb-1 text-[2.5rem] text-dark font-bold leading-none">{score}</span>
             </div>
         </div>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
       'sans': ['Barlow Semi Condensed']
     },
     extend: {
+      colors: {
+        dark: '#3b4363',
+        score: '#2a46c0',
+        'header-outline': '#606e85',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(circle at top, #1f3756, #141539)',
       }
