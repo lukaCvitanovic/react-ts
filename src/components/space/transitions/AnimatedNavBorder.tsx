fea2613ee62720312ebd3ed0dc538adf2169f6da
LukaCvitannovic <luka.cvitanovic@deptagency.com>
2022-02-24 10:25:42 +0100

    Added animation area to the AnimatedNavBorder - Animation area moves deppending on selected NavButton - AnimationArea works for horizontal and vertical animation

--- src/components/space/transitions/AnimatedNavBorder.tsx
+++ src/components/space/transitions/AnimatedNavBorder.tsx
@@ -1,20 +1,90 @@
 import { RefObject, useContext, useEffect, useLayoutEffect, useRef, useState } from "react";
 import { NavTransitionContext, initialElement } from "@/components/space/transitions/NavTransitionProvider";
 import { gsap } from 'gsap';
+import omit from "lodash/omit";
 
 type AnimatedNavBorderProps = {
     column?: boolean
 };
 
+type AnimatedAreaPosition = 
+    | { bottom: 0, left: number, right: number, column: false }
+    | { top: number, right: 0, column: true };
+
 const AnimatedNavBorder = ({ column = false }: AnimatedNavBorderProps) => {
     const { state: { navElement, initialNavElement }, dispatch } = useContext(NavTransitionContext);
+
+    const initialAAPosition: AnimatedAreaPosition = (column ? { top: 0, right: 0, column } : { bottom: 0, left: 0, right: 0, column });
+    const [AAPosition, setAAPosition] = useState<AnimatedAreaPosition>(initialAAPosition);
+    const [AADimension, setAADimension] = useState(0);
+    const [oldNavElement, setOldNavElement] = useState<Element | null>(null);
+
     const [initialWidth, setInitialWidth] = useState(0);
     const [initialX, setInitialX] = useState(0);
     const [oldXAxis, setOldXAxis] = useState(40);
     const [oldWidth, setOldWidht] = useState(0);
+
     const [layoutOffset, setLayoutOffset] = useState(0);
     
     const durationMS = 700;
+
+    const horizontalTransitionAnimationArea = () => {
+        const offsetFromRight = (element: HTMLElement) => element.offsetWidth + element.offsetLeft;
+        const navFromRight = offsetFromRight(navElement as HTMLElement);
+        const oldFromRight = offsetFromRight(oldNavElement as HTMLElement);
+
+        let widthExpansion;
+        const moveToRight = () => {
+            widthExpansion = navFromRight - oldFromRight;
+            setAADimension(oldWidth + widthExpansion);
+        };
+        const moveToLeft = () => {
+            widthExpansion = oldFromRight - navFromRight;
+            setAAPosition((currentPosition) => ({ ...currentPosition, left: (navElement as HTMLElement).offsetLeft }));
+            setAADimension((navElement as HTMLElement).offsetWidth + widthExpansion);
+        };
+
+        if (navFromRight > oldFromRight) moveToRight();
+        else moveToLeft();
+    };
+    const verticalTransitionAnimationArea = () => {
+        const offsetFromBottom = (element: HTMLElement) => element.offsetHeight + element.offsetTop;
+        const navFromBottom = offsetFromBottom(navElement as HTMLElement);
+        const oldFromBottom = offsetFromBottom(oldNavElement as HTMLElement);
+
+        let widthExpansion;
+        const moveDown = () => {
+            widthExpansion = navFromBottom - oldFromBottom;
+            setAADimension(oldWidth + widthExpansion);
+        };
+        const moveUp = () => {
+            widthExpansion = oldFromBottom - navFromBottom;
+            setAAPosition((currentPosition) => ({ ...currentPosition, top: (navElement as HTMLElement).offsetTop }));
+            setAADimension((navElement as HTMLElement).offsetHeight + widthExpansion);
+        };
+
+        if (navFromBottom > oldFromBottom) moveDown();
+        else moveUp();
+    };
+    const transitionAnimationArea = () => {
+        if (column) verticalTransitionAnimationArea();
+        else horizontalTransitionAnimationArea();
+    };
+
+    const horizontalStartEndAnimationArea = () => {
+        setAADimension((navElement as HTMLElement).offsetWidth);
+        setAAPosition((currentPosition) => ({ ...currentPosition, left: (navElement as HTMLElement).offsetLeft }));
+        setOldNavElement(navElement);
+    };
+    const verticalStartEndAnimationArea = () => {
+        setAADimension((navElement as HTMLElement).offsetHeight);
+        setAAPosition((currentPosition) => ({ ...currentPosition, top: (navElement as HTMLElement).offsetTop }));
+        setOldNavElement(navElement);
+    };
+    const startEndAnimationArea = () => {
+        if (column) verticalStartEndAnimationArea();
+        else horizontalStartEndAnimationArea();
+    };
 
     const gsapSlidingAnimation = (ref: RefObject<HTMLHRElement>) => {
         const timeline = gsap.timeline({ onComplete: onAnimationEnd });
@@ -101,6 +171,7 @@ const AnimatedNavBorder = ({ column = false }: AnimatedNavBorderProps) => {
     };
 
     const onAnimationEnd = () => {
+        startEndAnimationArea();
         setOldXAxis((column ? navElement.getBoundingClientRect().y : navElement.getBoundingClientRect().x));
         setOldWidht((column ? navElement.getBoundingClientRect().height : navElement.getBoundingClientRect().width));
     }
@@ -124,36 +195,54 @@ const AnimatedNavBorder = ({ column = false }: AnimatedNavBorderProps) => {
         };
     }, [initialNavElement.getBoundingClientRect().x, initialNavElement.getBoundingClientRect().y])
     useEffect(() => {
+        if (oldNavElement !== null) transitionAnimationArea();
         // TO Enable animation
         if (initialWidth && initialX) animateSlider();
-        // console.log(navElement.getBoundingClientRect());
     }, [navElement.getBoundingClientRect().x, navElement.getBoundingClientRect().y]);
-
+    useEffect(() => {
+        if (!initialNavElement.isEqualNode(initialElement)) {
+            setAAPosition((currentPosition) => ({ ...currentPosition, left: (initialNavElement as HTMLElement).offsetLeft }));
+            setAADimension((initialNavElement as HTMLElement).offsetWidth);
+        }
+    }, [initialNavElement]);
     
     useLayoutEffect(() => {
         let offset = 0;
-        let a;
-        if (!column) offset = ref.current?.getBoundingClientRect().x || 0;
         setLayoutOffset(offset);
     }, []);
 
     const topBottom = (column ? { top: 0, right: 0, marginTop: '' } : { bottom: 0 });
 
+    const toOmit = (column ? ['column', 'left'] : ['column']);
+    const style = {
+        backgroundColor: 'red',
+        width: `${(column ? 3 : AADimension)}px`,
+        height: `${(column ? AADimension : 3)}px`,
+        ...omit(AAPosition, toOmit),
+    };
+
     return (
-        <hr
-            ref={ref}
+        <div
             style={{
                 position: 'absolute',
-                ...topBottom,
-                borderTopWidth: `${(column ? 0 : '3px')}`,
-                borderTopColor: 'white',
-                borderLeftWidth: `${(column ? '3px' : 0)}`,
-                borderLeftColor: 'white',
-                width: `${(column ? 3 : initialWidth)}px`,
-                height: `${(column ? initialWidth : 3)}px`,
-                transform: `translate${column ? 'Y' : 'X'}(${initialX - layoutOffset}px)`
+                ...style,
             }}
-        />
+        >
+            <hr
+                ref={ref}
+                style={{
+                    position: 'absolute',
+                    ...topBottom,
+                    borderTopWidth: `${(column ? 0 : '3px')}`,
+                    borderTopColor: 'white',
+                    borderLeftWidth: `${(column ? '3px' : 0)}`,
+                    borderLeftColor: 'white',
+                    width: `${(column ? 3 : initialWidth)}px`,
+                    height: `${(column ? initialWidth : 3)}px`,
+                    transform: `translate${column ? 'Y' : 'X'}(${initialX - layoutOffset}px)`
+                }}
+            />
+        </div>
     );
 };
 
